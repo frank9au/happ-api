425f05e920fdb6976f92ba6903de32160474314d
wenmingliang <wenmingliang@bjebc.com>
2023-03-06 11:01:05 +0800

    项目初始化-2

--- src/main/java/com/happy/HappyApiApplication.java
+++ src/main/java/com/happy/HappyApiApplication.java
@@ -1,10 +1,12 @@
 package com.happy;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+@Slf4j
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, DruidDataSourceAutoConfigure.class})
 public class HappyApiApplication {
 
@@ -15,5 +17,4 @@ public class HappyApiApplication {
             e.printStackTrace();
         }
     }
-
 }
